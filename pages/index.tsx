f637fb59314c380be968774623ad6ca893ea6654
Theryston <fuktodo2@gmail.com>
2022-12-19 16:06:52 -0300

    feat: text gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import { Spacer, Text } from '@nextui-org/react';
+import { Grid, Image, Text } from '@nextui-org/react';
+import WebFriendsImg from '../assets/web-friends.jpg';
 import { Box } from '../components/Box';
 
 export default function Home() {
@@ -13,8 +14,36 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Box>
-        <Text h1>Em-breve!</Text>
+
+      <Box
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          position: 'relative',
+        }}
+      >
+        <Text
+          h1
+          css={{
+            textGradient: '45deg, $blue600 -20%, $pink600 100%',
+          }}
+          weight="bold"
+          size={60}
+        >
+          Free Chat
+        </Text>
+        <Text h3>Fale com desconhecidos!</Text>
+        <Text
+          size="$lg"
+          css={{
+            textAlign: 'center',
+          }}
+        >
+          Free Chat é um serviço de bate-papo anônimo gratuito que permite que você converse com desconhecidos de todo o
+          mundo. Experimente conversas divertidas e interessantes, conheça pessoas novas, faça amigos, discuta sobre
+          assuntos variados e crie memórias únicas.
+        </Text>
       </Box>
     </>
   );
